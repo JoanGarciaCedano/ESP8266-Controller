4b3e8a31645ecf5e97642823bf1b1965e6c2fb9f
Cloors <cloors@outlook.com>
2019-02-24 12:00:03 -0700

    JoanGC - Cambios Index Server

--- servidor/index.js
+++ servidor/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 //Cargamos el paquete express
-var express = require('express');
+const express = require('express');
 //Cargamos el paquete bodyParser
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 //Llamamos a Express
-var app = express();
-var port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 //Usamos BodyParser en nuestra APP
 app.use(bodyParser.urlencoded({extended:false}));
@@ -15,8 +15,21 @@ app.use(bodyParser.urlencoded({extended:false}));
 //Parseamos lo que venga por la URLENCODED en JSON
 app.use(bodyParser.json());
 
+//Creamos las rutas
+app.get('/prueba/:nombre?', (req, res) =>{   
+
+   const { nombre = 'SIN NOMBRE' } = req.params;
+    res.status(200).json({
+      data: [2, 3, 4],
+      mensaje: `respuesta de URL /prueba ${nombre}`
+    });
+});
+
+
+
 //Creamos el servidor
 app.listen(port, () =>{
-    console.log(`Servidor ejecutandose en http://localhost:${port}`);
+    console.log(`Servidor ejecutandose en http://localho|st:${port}`);
     console.log("Nodemon funcionando");
 });
+
